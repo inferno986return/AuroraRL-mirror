3fe4902c9dfbb5c3bc2670ff06123c8cdf0d3612
esmirnov <esmirnov@localhost>
2013-06-25 21:01:22 +0400

    Updated launch scripts with nifty-gui classpath

--- scripts/game.bat
+++ scripts/game.bat
@@ -1,1 +1,1 @@
-java -cp "resources;Aurora.jar;Libnoiseforjava-master.jar;TWL.jar" ru.game.aurora.application.AuroraGame
+java -cp "resources;Aurora.jar;Libnoiseforjava-master.jar;nifty-1.3.2.jar;nifty-default-controls-1.3.2.jar;nifty-slick-renderer-1.3.2.jar;nifty-style-black-1.3.2.jar;eventbus-1.4.jar" ru.game.aurora.application.AuroraGame
