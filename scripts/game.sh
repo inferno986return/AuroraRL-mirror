393f8302a6ca1c586cb15880651edc1a93fd3366
e_smirnov <e_smirnov@localhost>
2013-08-15 19:04:43 +0400

    Added forgotten files. Fixed manifes. Now game can be launched by just clicking on Aurora.jar

--- scripts/game.sh
+++ scripts/game.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp "resources:frankenstein-core.jar:Aurora.jar:Libnoiseforjava-master.jar:nifty-1.3.2.jar:nifty-default-controls-1.3.2.jar:nifty-slick-renderer-1.3.2.jar:nifty-style-black-1.3.2.jar:eventbus-1.4.jar" ru.game.aurora.application.AuroraGame
+java -cp "resources:." -jar Aurora.jar
