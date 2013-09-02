90f99a6e27f5f9901eb4834c48fa378ad449a4e1
e_smirnov <e_smirnov@localhost>
2013-09-02 17:40:44 +0400

    Added dialog and icon to encounter with UNS Enterprise

--- src/ru/game/aurora/world/generation/humanity/EnterpriseEncounterCreator.java
+++ src/ru/game/aurora/world/generation/humanity/EnterpriseEncounterCreator.java
@@ -8,6 +8,10 @@
 package ru.game.aurora.world.generation.humanity;
 
 import ru.game.aurora.application.CommonRandom;
+import ru.game.aurora.application.GameLogger;
+import ru.game.aurora.dialog.Dialog;
+import ru.game.aurora.dialog.DialogListener;
+import ru.game.aurora.npc.NPC;
 import ru.game.aurora.npc.SingleShipEvent;
 import ru.game.aurora.player.earth.PrivateMessage;
 import ru.game.aurora.world.GameEventListener;
@@ -32,6 +36,26 @@ public class EnterpriseEncounterCreator extends GameEventListener
         turnCount = CommonRandom.getRandom().nextInt(50) + 50;
     }
 
+    private static class EnterpriseDialogListener implements DialogListener
+    {
+
+        private static final long serialVersionUID = 8961118747394070541L;
+
+        @Override
+        public void onDialogEnded(World world, int returnCode) {
+            if (returnCode == 1) {
+                // repair
+                final int maxHull = world.getPlayer().getShip().getMaxHull();
+                if (world.getPlayer().getShip().getHull() == maxHull) {
+                    GameLogger.getInstance().logMessage("Ship's hull is not damaged");
+                } else {
+                    GameLogger.getInstance().logMessage("Enterprise engineers helped our crew in fixing breaches in ship hull");
+                    world.getPlayer().getShip().setHull(maxHull);
+                }
+            }
+        }
+    }
+
     @Override
     public void onTurnEnded(World world) {
         if (!isAlive) {
@@ -44,24 +68,27 @@ public class EnterpriseEncounterCreator extends GameEventListener
         // spawn private messages.
         world.getPlayer().getEarthState().getMessages().add(new PrivateMessage(
                 "At last",
-                "Hi, man! \n At last my time on Earth is over, they have finally finished building my ship and we are leaving in a couple of days." +
-                        " And guess how they named her? Enterprise, like in that old movie, haha. \n" +
-                        " So now we are joining you in your search. Looking forward to meet you in outer space, man. \n" +
-                        " Derek McCartney, captain of UNS Enterprise, lol.",
+                "Hello! \n At last my time on Earth is over, they have finally finished building my ship and we are leaving in a couple of days. So don't think you will be ahead of your teacher anymore." +
+                        " And guess how they named her? Enterprise, like in that old movie, heh. \n" +
+                        " So now we are joining you in your search. Looking forward to meet you in outer space, boy. \n" +
+                        " Derek McCartney, captain of UNS Enterprise, can't stop laughing from that name.",
                 "message"
         ));
 
         world.getPlayer().getEarthState().getMessages().add(new PrivateMessage("Aurora project news: new ship launched"
         , "A new ship from the Aurora project has been successfully launched this friday. It will explore new sector of galaxy near the Orion star system, its main" +
                 " objective is establishing diplomatic contacts with alien species in this region. \n " +
-                " Ship is called UNS Enterprise, based on hundreds of thousands plees, received from Star Trek fans. It is the third ship in this series, capable of carrying up to a " +
-                "thousand crewmembers, scientists and explorers." +
+                " Ship is called UNS Enterprise, based on hundreds of thousands pleas, received from Star Trek fans. It is the third ship in this series, capable of carrying up to a " +
+                "thousand crew members, scientists and explorers." +
                 "\n It return is scheduled for autumn of a next year.", "news"));
 
 
-        NPCShip enterprise = new NPCShip(0, 0, "aurora", world.getRaces().get("Humanity"), null, "UNS Enterprise");
+        Dialog captainDialog = Dialog.loadFromFile("dialogs/encounters/enterprise.json");
+        captainDialog.setListener(new EnterpriseDialogListener());
 
-        world.addListener(new SingleShipEvent(0.1, enterprise));
+        NPCShip enterprise = new NPCShip(0, 0, "aurora", world.getRaces().get("Humanity"), new NPC(captainDialog), "UNS Enterprise");
+
+        world.addListener(new SingleShipEvent(0.9, enterprise));
 
         isAlive = false;
     }
