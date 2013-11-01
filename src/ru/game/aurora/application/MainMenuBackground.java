17b16c0c224bb183a1a09ac6406988871e96510d
dname ksurname <rndmn163@gmail.com>
2013-11-01 20:44:06 +0300

    Added small sprites for stars on background

--- src/ru/game/aurora/application/MainMenuBackground.java
+++ src/ru/game/aurora/application/MainMenuBackground.java
@@ -1,11 +1,16 @@
 package ru.game.aurora.application;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import ru.game.aurora.util.CollectionUtils;
+import ru.game.aurora.util.EngineUtils;
 import ru.game.aurora.world.space.ParallaxBackground;
 import ru.game.aurora.world.space.StarSystem;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -18,18 +23,67 @@ public class MainMenuBackground {
 
     private ParallaxBackground.Star[][] stars;
 
+    private static final int STARS_PER_PLANE = 30;
+
     private long elapsed;
 
     private int width;
 
+    Image s0; //sprites for stars
+    Image s1;
+    Image s2;
+    Image s3;
+
     public MainMenuBackground(int width, int height) {
+        s0 = ResourceManager.getInstance().getImage("stars_s0");
+        s1 = ResourceManager.getInstance().getImage("stars_s1");
+        s2 = ResourceManager.getInstance().getImage("stars_s2");
+        s3 = ResourceManager.getInstance().getImage("stars_s3");
         this.width = width;
         stars = new ParallaxBackground.Star[ParallaxBackground.PLANES_COUNT][ParallaxBackground.STARS_PER_PLANE];
         final Random random = CommonRandom.getRandom();
 
         for (int i = 0; i < ParallaxBackground.PLANES_COUNT; ++i) {
-            for (int j = 0; j < ParallaxBackground.STARS_PER_PLANE; ++j) {
-                stars[i][j] = new ParallaxBackground.Star(random.nextInt(width), random.nextInt(height), CollectionUtils.selectRandomElement(StarSystem.possibleColors), random.nextInt(3) + 1);
+            for (int j = 0; j < STARS_PER_PLANE; ++j) {
+                ParallaxBackground.Star newStar;
+                Image star_image;
+                Map<Color, Color> colorMap = new HashMap<>();
+
+                newStar = new ParallaxBackground.Star(random.nextInt(width), random.nextInt(height), CollectionUtils.selectRandomElement(StarSystem.possibleColors), random.nextInt(3) + 1);
+
+                //select colors for star
+                if (newStar.color == Color.red) {
+                    colorMap.put(new Color(0, 113, 61, 255), new Color(95,29,18));
+                    colorMap.put(new Color(134, 197, 86, 255), new Color(221,102,61));
+                } else if (newStar.color == Color.white) {
+                    colorMap.put(new Color(0, 113, 61, 255), new Color(56,56,56));
+                    colorMap.put(new Color(134, 197, 86, 255), new Color(141,141,141));
+                } else if (newStar.color == Color.yellow) {
+                    colorMap.put(new Color(0, 113, 61, 255), new Color(113,91,0));
+                    colorMap.put(new Color(134, 197, 86, 255), new Color(194,190,57));
+                } else {
+                    colorMap.put(new Color(0, 113, 61, 255), new Color(0,42,113));
+                    colorMap.put(new Color(134, 197, 86, 255), new Color(86,128,197));
+                }
+
+                //select sprite for star
+                switch (i) {
+                    case 2:
+                        star_image = EngineUtils.replaceColors(s2, colorMap);
+                        break;
+                    case 3:
+                        star_image = EngineUtils.replaceColors(s1, colorMap);
+                        break;
+                    case 4:
+                        star_image = EngineUtils.replaceColors(s0, colorMap);
+                        break;
+                    default:
+                        star_image = EngineUtils.replaceColors(s3, colorMap);
+                        break;
+                }
+
+                newStar.setSprite(star_image);
+                stars[i][j] = newStar;
             }
         }
     }
@@ -38,24 +92,21 @@ public class MainMenuBackground {
         elapsed += (container.getTime() - AuroraGame.getLastFrameTime());
         if (elapsed >= 200) {
             for (int i = 0; i < ParallaxBackground.PLANES_COUNT; ++i) {
-                for (int j = 0; j < ParallaxBackground.STARS_PER_PLANE; ++j) {
+                for (int j = 0; j < STARS_PER_PLANE; ++j) {
                     stars[i][j].x -= 1.0 / (i + 1);
                     if (stars[i][j].x < -5) {
                         stars[i][j].x = width + 5;
                     }
                 }
             }
-
         }
     }
 
     public void draw(Graphics graphics) {
         for (int i = 0; i < ParallaxBackground.PLANES_COUNT; ++i) {
-            for (int j = 0; j < ParallaxBackground.STARS_PER_PLANE; ++j) {
+            for (int j = 0; j < STARS_PER_PLANE; ++j) {
                 final ParallaxBackground.Star s = stars[i][j];
-                graphics.setColor(s.color);
-                float diameter = Math.max(1, 2 * (float) Math.ceil(s.baseSize / (2 * i + 1)));
-                graphics.fillOval(s.x, s.y, diameter, diameter);
+                graphics.drawImage(s.sprite,s.x,s.y);
             }
         }
     }
--- src/ru/game/aurora/world/space/ParallaxBackground.java
+++ src/ru/game/aurora/world/space/ParallaxBackground.java
@@ -9,9 +9,14 @@ package ru.game.aurora.world.space;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import ru.game.aurora.application.Camera;
+import ru.game.aurora.application.ResourceManager;
 import ru.game.aurora.util.CollectionUtils;
+import ru.game.aurora.util.EngineUtils;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class ParallaxBackground {
@@ -24,12 +29,16 @@ public class ParallaxBackground {
         public float y;
         public Color color;
         public int baseSize;
+        public Image sprite;
 
         public Star(float x, float y, Color color, int baseSize) {
             this.x = x;
             this.y = y;
             this.color = color;
             this.baseSize = baseSize;
+        }
+        public void setSprite(Image sprite) {
+            this.sprite = sprite;
         }
     }
 
@@ -44,7 +53,16 @@ public class ParallaxBackground {
 
     private float baseWidth = 3;
 
+    Image s0; //sprites for stars
+    Image s1;
+    Image s2;
+    Image s3;
+
     public ParallaxBackground(float width, float height, float centerX, float centerY, int density) {
+        s0 = ResourceManager.getInstance().getImage("stars_s0");
+        s1 = ResourceManager.getInstance().getImage("stars_s1");
+        s2 = ResourceManager.getInstance().getImage("stars_s2");
+        s3 = ResourceManager.getInstance().getImage("stars_s3");
         if (density == 0) {
             density = 1;
         }
@@ -52,7 +70,45 @@ public class ParallaxBackground {
         stars = new Star[PLANES_COUNT][starsPerPlane];
         for (int i = 0; i < PLANES_COUNT; ++i) {
             for (int j = 0; j < starsPerPlane; ++j) {
-                stars[i][j] = new Star(r.nextFloat() * 2 * width - centerX - width, r.nextFloat() * 2 * height - centerY - height, CollectionUtils.selectRandomElement(StarSystem.possibleColors), r.nextInt((int) baseWidth) + 1);
+                Star newStar;
+                Image star_image;
+                Map<Color, Color> colorMap = new HashMap<>();
+
+                newStar = new Star(r.nextFloat() * 2 * width - centerX - width, r.nextFloat() * 2 * height - centerY - height, CollectionUtils.selectRandomElement(StarSystem.possibleColors), r.nextInt((int) baseWidth) + 1);
+
+                //select colors for star
+                if (newStar.color == Color.red) {
+                    colorMap.put(new Color(0, 113, 61, 255), new Color(95,29,18));
+                    colorMap.put(new Color(134, 197, 86, 255), new Color(221,102,61));
+                } else if (newStar.color == Color.white) {
+                    colorMap.put(new Color(0, 113, 61, 255), new Color(56,56,56));
+                    colorMap.put(new Color(134, 197, 86, 255), new Color(141,141,141));
+                } else if (newStar.color == Color.yellow) {
+                    colorMap.put(new Color(0, 113, 61, 255), new Color(113,91,0));
+                    colorMap.put(new Color(134, 197, 86, 255), new Color(194,190,57));
+                } else {
+                    colorMap.put(new Color(0, 113, 61, 255), new Color(0,42,113));
+                    colorMap.put(new Color(134, 197, 86, 255), new Color(86,128,197));
+                }
+
+                //select sprite for star
+                switch (i) {
+                    case 2:
+                        star_image = EngineUtils.replaceColors(s2, colorMap);
+                        break;
+                    case 3:
+                        star_image = EngineUtils.replaceColors(s1, colorMap);
+                        break;
+                    case 4:
+                        star_image = EngineUtils.replaceColors(s0, colorMap);
+                        break;
+                    default:
+                        star_image = EngineUtils.replaceColors(s3, colorMap);
+                        break;
+                }
+
+                newStar.setSprite(star_image);
+                stars[i][j] = newStar;
             }
         }
     }
@@ -76,15 +132,7 @@ public class ParallaxBackground {
                 Star s = stars[i][j];
                 int realX = (int) getXCoordPoint(camera, (int) s.x, i);
                 int realY = (int) getYCoordPoint(camera, (int) s.y, i);
-
-                graphics.setColor(s.color);
-
-                float diameter = Math.max(1, 2 * (float) Math.ceil(s.baseSize / (2 * i + 1)));
-
-                if (realX < -diameter || realY < -diameter || realX > camera.getTileWidth() * camera.getNumTilesX() || realY > camera.getTileHeight() * camera.getNumTilesY()) {
-                    continue;
-                }
-                graphics.fillOval(realX, realY, diameter, diameter);
+                graphics.drawImage(s.sprite,realX,realY);
             }
         }
     }
