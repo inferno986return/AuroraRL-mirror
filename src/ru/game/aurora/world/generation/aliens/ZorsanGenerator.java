9a71309db0f74299a906236d30dde3fb81fbf2ea
esmirnov <esmirnov@localhost>
2014-01-19 22:04:45 +0400

    Resources for a zorsan race

--- src/ru/game/aurora/world/generation/aliens/ZorsanGenerator.java
+++ src/ru/game/aurora/world/generation/aliens/ZorsanGenerator.java
@@ -11,13 +11,12 @@ import ru.game.aurora.world.generation.WorldGeneratorPart;
  * Date: 25.12.13
  * Time: 18:17
  */
-public class ZorsanGenerator implements WorldGeneratorPart
-{
+public class ZorsanGenerator implements WorldGeneratorPart {
     private static final long serialVersionUID = 1083992211652099884L;
 
     @Override
     public void updateWorld(World world) {
-        AlienRace race = new AlienRace("Zorsan", "klisk_ship", Dialog.loadFromFile("dialogs/zorsan_main.json"));
+        AlienRace race = new AlienRace("Zorsan", "zorsan_scout", Dialog.loadFromFile("dialogs/zorsan_main.json"));
         world.getRaces().put(race.getName(), race);
     }
 }
