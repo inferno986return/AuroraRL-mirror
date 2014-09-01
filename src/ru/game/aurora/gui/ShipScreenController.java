824d576a0aa261f0dac0a25c794a3772cb6a3a14
esmirnov <esmirnov@localhost>
2014-09-01 19:45:46 +0400

    Improved ship screen

--- src/ru/game/aurora/gui/ShipScreenController.java
+++ src/ru/game/aurora/gui/ShipScreenController.java
@@ -3,10 +3,12 @@ package ru.game.aurora.gui;
 import com.google.common.collect.Multiset;
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.controls.ListBox;
+import de.lessvoid.nifty.elements.Element;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 import ru.game.aurora.npc.CrewMember;
 import ru.game.aurora.player.engineering.ShipUpgrade;
+import ru.game.aurora.util.EngineUtils;
 import ru.game.aurora.world.Ship;
 import ru.game.aurora.world.World;
 import ru.game.aurora.world.planet.InventoryItem;
@@ -25,6 +27,10 @@ public class ShipScreenController implements ScreenController {
 
     private Screen myScreen;
 
+    private Element credCountElement;
+
+    private Element resCountElement;
+
     public ShipScreenController(World world) {
         this.world = world;
     }
@@ -33,8 +39,12 @@ public class ShipScreenController implements ScreenController {
     public void bind(Nifty nifty, Screen screen) {
         crewMemberListBox = screen.findNiftyControl("crew", ListBox.class);
         modulesListBox = screen.findNiftyControl("modules", ListBox.class);
-        inventory = screen.findNiftyControl("inventory", ListBox.class);
+        inventory = screen.findNiftyControl("items", ListBox.class);
         myScreen = screen;
+
+        credCountElement = myScreen.findElementByName("cred_count").findElementByName("#count");
+        resCountElement = myScreen.findElementByName("res_count").findElementByName("#count");
+
     }
 
     @Override
@@ -55,6 +65,9 @@ public class ShipScreenController implements ScreenController {
 
         myScreen.layoutLayers();
         world.setPaused(true);
+
+        EngineUtils.setTextForGUIElement(resCountElement, String.valueOf(world.getPlayer().getResourceUnits()));
+        EngineUtils.setTextForGUIElement(credCountElement, String.valueOf(world.getPlayer().getCredits()));
     }
 
     @Override
