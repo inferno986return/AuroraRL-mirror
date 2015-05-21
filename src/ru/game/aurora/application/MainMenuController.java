e2f09828ebc9bb26a7498d0b53f8057bcd23074d
esmirnov <esmirnov@localhost>
2015-05-21 19:03:09 +0300

    Fixed blog address in russian version. Removed some obsolete files

--- src/ru/game/aurora/application/MainMenuController.java
+++ src/ru/game/aurora/application/MainMenuController.java
@@ -110,7 +110,11 @@ public class MainMenuController implements ScreenController, ResolutionChangeLis
     public void openBlog() {
         if (Desktop.isDesktopSupported()) {
             try {
-                Desktop.getDesktop().browse(URI.create("http://auroraroguelike.com"));
+                if (Localization.getCurrentLocaleTag().equalsIgnoreCase("ru")) {
+                    Desktop.getDesktop().browse(URI.create("http://auroraroguelike.ru"));
+                } else {
+                    Desktop.getDesktop().browse(URI.create("http://auroraroguelike.com"));
+                }
             } catch (IOException e) {
                 logger.error("Failed to open blog uri", e);
             }
