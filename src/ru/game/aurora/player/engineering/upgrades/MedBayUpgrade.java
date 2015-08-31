c13dc810b50bf014f97d58ad829ae3dd38c3dabd
e_smirnov <e_smirnov@localhost>
2015-08-31 20:12:50 +0300

    Super medbay upgrade

--- src/ru/game/aurora/player/engineering/upgrades/MedBayUpgrade.java
+++ src/ru/game/aurora/player/engineering/upgrades/MedBayUpgrade.java
@@ -1,11 +1,11 @@
 package ru.game.aurora.player.engineering.upgrades;
 
-import java.util.List;
-
 import ru.game.aurora.application.Configuration;
 import ru.game.aurora.player.engineering.ShipUpgrade;
 import ru.game.aurora.world.Ship;
 import ru.game.aurora.world.World;
+
+import java.util.List;
 
 public class MedBayUpgrade extends ShipUpgrade {
     
@@ -29,15 +29,29 @@ public class MedBayUpgrade extends ShipUpgrade {
         this(false);
     }
 
+    public static int getCrewDeathReduceValue() {
+        List<ShipUpgrade> upgrades = World.getWorld().getPlayer().getShip().getUpgrades();
+
+        int reduce = 0;
+        for (ShipUpgrade upgrade : upgrades) {
+            if (upgrade instanceof MedBayUpgrade) {
+                reduce += ((MedBayUpgrade) upgrade).enhanced
+                        ? reduceCrewDeathEnhanced : reduceCrewDeath;
+            }
+        }
+
+        return reduce;
+    }
+
     @Override
     public void onInstalled(World world, Ship ship) {
-        
+
 
     }
 
     @Override
     public void onRemoved(World world, Ship ship) {
-        
+
 
     }
 
@@ -46,17 +60,22 @@ public class MedBayUpgrade extends ShipUpgrade {
         return Configuration.getIntProperty("upgrades.medbay.size");
     }
 
-    public static int getCrewDeathReduceValue() {
-        List<ShipUpgrade> upgrades = World.getWorld().getPlayer().getShip().getUpgrades();
-        
-        int reduce = 0;
-        for(ShipUpgrade upgrade : upgrades) {
-            if(upgrade instanceof MedBayUpgrade) {
-                reduce += ((MedBayUpgrade)upgrade).enhanced 
-                        ? reduceCrewDeathEnhanced : reduceCrewDeath;
-            }
-        }
-        
-        return reduce;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        MedBayUpgrade that = (MedBayUpgrade) o;
+
+        return enhanced == that.enhanced;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (enhanced ? 1 : 0);
+        return result;
     }
 }
